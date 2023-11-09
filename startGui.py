8f4fac55d64eca065bd143f413cc5180bcaf95df
JB <j.bishop@utoronto.ca>
2023-11-09 16:25:37 -0500

    corrected location of start script

--- startGui.py
+++ startGui.py
@@ -1,4 +1,4 @@
-import sys
+import sys,os
 import argparse
 
 from src.Gui import Gui
@@ -15,11 +15,5 @@ def main():
     gui = Gui(optionsFlag, debug=args.debug)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--debug',action = 'store_true',default=False)
-    parser.add_argument('--options',default='0')
-    args = parser.parse_args()
-    optionsFlag = int(args.options)
 
     main()
-    # main(optionsFlag,debug=args.debug)
