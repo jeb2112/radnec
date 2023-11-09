3c82735cd30d31b82761f030882a5393c2bae079
JB <j.bishop@utoronto.ca>
2023-11-09 17:13:59 -0500

    have to duplicate startGui.py in order for script to run from both a package installed to venv, and from source code in debugger

---
+++ src/startGui.py
@@ -0,0 +1,19 @@
+import sys,os
+import argparse
+
+from src.Gui import Gui
+
+# def main(optionsFlag,debug=False):
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug',action = 'store_true',default=False)
+    parser.add_argument('--options',default='0')
+    args = parser.parse_args()
+    optionsFlag = int(args.options)
+
+    gui = Gui(optionsFlag, debug=args.debug)
+
+if __name__ == '__main__':
+
+    main()
