2fb236b8feb5f6fd98c44686303cb081378d1a0f
unknown <j.bishop@utoronto.ca>
2024-06-04 15:18:10 -0400

    updated nnunet environment

--- src/DcmCase.py
+++ src/DcmCase.py
@@ -924,12 +924,12 @@ class DcmStudy(Study):
                 activatebatch = "C:\Program Files\\anaconda3\Scripts\\activate.bat"
             else:
                 raise FileNotFoundError('anaconda3/Scripts/activate.bat')
-            if os.path.isdir(os.path.expanduser('~')+'\\anaconda3\envs\\pytorch117_310'):
-                envpath = os.path.expanduser('~')+'\\anaconda3\envs\\pytorch117_310'
-            elif os.path.isdir(os.path.expanduser('~')+'\\.conda\envs\\pytorch117_310'):
-                envpath = os.path.expanduser('~')+'\\.conda\envs\\pytorch117_310'
+            if os.path.isdir(os.path.expanduser('~')+'\\anaconda3\envs\\pytorch118_310'):
+                envpath = os.path.expanduser('~')+'\\anaconda3\envs\\pytorch118_310'
+            elif os.path.isdir(os.path.expanduser('~')+'\\.conda\envs\\pytorch118_310'):
+                envpath = os.path.expanduser('~')+'\\.conda\envs\\pytorch118_310'
             else:
-                raise FileNotFoundError('pytorch117_310')
+                raise FileNotFoundError('pytorch118_310')
 
             command1 = '\"'+activatebatch+'\" \"' + envpath + '\"'
             command2 = 'nnUNetv2_predict -i \"' + dpath + '\" -o \"' + dpath + '\" -d137 -c 3d_fullres'
