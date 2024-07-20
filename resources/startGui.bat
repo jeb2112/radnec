8578c2e7ef610e518ad1c5308e99ef6ffa2d9ccd
JB <j.bishop@utoronto.ca>
2024-07-20 13:14:40 -0400

    updating wheel build

--- resources/startGui.bat
+++ resources/startGui.bat
@@ -1,5 +1,5 @@
 :: @echo off
-call "C:\Program Files\anaconda3\Scripts\activate.bat" "C:\Users\Chris Heyn Lab\.conda\envs\blast"
-cd "C:\Users\Chris Heyn Lab\src\blast"
+call "C:\Program Files\anaconda3\Scripts\activate.bat" "C:\Users\Chris Heyn Lab\anaconda3\envs\blast"
+cd "C:\Users\Chris Heyn Lab\src\radnec"
 python src\startGui.py
 exit
