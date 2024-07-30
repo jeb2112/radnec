7acd6e29e5e4369f8b84ce0b2ae1c4b04a202858
unknown <j.bishop@utoronto.ca>
2024-07-30 18:01:01 -0400

    fix script location

--- resources/installGui.bat
+++ resources/installGui.bat
@@ -1,5 +1,5 @@
 @REM installation script for RadNec SAM viewer .whl file
-@REM .whl file should be in the Userprofile/src/whl directory
+@REM .whl file should be in the same directory as this script
 @REM if multiple .whl files are present, the one with the highest revision number
 @REM will be installed according to a simple sort
 
@@ -7,15 +7,13 @@ call "C:\Program Files\anaconda3\Scripts\activate.bat" "%USERPROFILE%\anaconda3"
 @echo off
 echo Installing RadNec/SAM viewer...
 timeout /t 2 > nul
-@REM @echo on
+@echo on
 call conda create --solver=libmamba -n radnec_sam -c conda-forge cupy python=3.9 cuda-version=11.8
-@REM call conda create --solver=libmamba -n radnec_sam -c conda-forge python=3.9
 call conda activate radnec_sam
 @ echo off
-cd "%userprofile%\src\whl"
-@REM @ echo on
-dir /b/o:-n > whls.txt
-for /f "skip=1" %%a in (whls.txt) do (
+cd %~dp0
+dir /b/o:-n *.whl > whls.txt
+for /f %%a in (whls.txt) do (
     echo Installing %%a ...
     @echo on
     call pip install %%a
@@ -24,7 +22,21 @@ for /f "skip=1" %%a in (whls.txt) do (
     goto next
 )
 :next
+call conda deactivate
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\RadNecSAM.lnk');$s.IconLocation='%USERPROFILE%\anaconda3\envs\radnec_sam\Lib\site-packages\resources\sunnybrook.ico';$s.WorkingDirectory='C:\Users\Chris Heyn Lab\anaconda3\envs\radnec_sam\Lib\site-packages\resources';$s.TargetPath='C:\Users\Chris Heyn Lab\anaconda3\envs\radnec_sam\Lib\site-packages\resources\startGui.bat';$s.Save()"
+
+echo
+echo Installing the SAM, nnUNet code...
+timeout /t 2 > nul
+@echo on
+call conda create -n pytorch_sam python=3.10
+call conda activate pytorch_sam
+call conda install numpy
+call conda install --solver=libmamba pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+call pip install git+https://github.com/facebookresearch/segment-anything.git
+call pip install nnunetv2
+call conda deactivate
+
 echo Installation complete, exiting...
 timeout /t 4 > nul
 exit
