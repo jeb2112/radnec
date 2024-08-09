9dab9ff50d180a5570047e047fa94c436195e499
unknown <j.bishop@utoronto.ca>
2024-08-09 11:08:34 -0400

    fix env variable error

--- resources/startGui.bat
+++ resources/startGui.bat
@@ -1,15 +1,14 @@
 @echo off
-set user=%USERPROFILE%
-if  exist "%user%\anaconda3" ( 
-    call "C:\Program Files\anaconda3\Scripts\activate.bat" "%user%\anaconda3\envs\radnec_sam"
+if  exist "%USERPROFILE%\anaconda3" ( 
+    call "C:\Program Files\anaconda3\Scripts\activate.bat" "%USERPROFILE%\anaconda3\envs\radnec_sam"
 ) else if exist "C:\Program Files\anaconda3" (
-    call "%user%\Scripts\activate.bat" "%user%\anaconda3\envs\radnec_sam"
+    call "%USERPROFILE%\Scripts\activate.bat" "%USERPROFILE%\anaconda3\envs\radnec_sam"
 ) else (
     echo "anaconda not found, exiting..."
     pause
     exit
 )
-cd "%user%\anaconda3\envs\radnec_sam\Lib\site-packages"
+cd "%USERPROFILE%\anaconda3\envs\radnec_sam\Lib\site-packages"
 @echo on
 python src\startGui.py
 @echo off
