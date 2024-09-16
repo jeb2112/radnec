38a72602a8a67e2e886a4bde19209ccf300bb5ea
JB <j.bishop@utoronto.ca>
2024-09-16 19:42:40 -0400

    modified sam_hf.py script to use the torch DataLoader

--- scripts/sam_hf.py
+++ scripts/sam_hf.py
@@ -1,28 +1,30 @@
-# script for loading sam checkpoint results and running predictions
-# uses the huggingface classes
+# script for loading checkpoint results 
+# for sam fine-tuning with BraTS2024
 
 import numpy as np
-import argparse
 import os
 import re
 import matplotlib.pyplot as plt
 from sklearn.metrics import f1_score, precision_score
 from scipy.spatial.distance import dice,directed_hausdorff
 from collections import defaultdict
-import json
 import copy
+import argparse
 
 import skimage
 from skimage.io import imread,imsave
 from skimage.transform import resize
-import pandas as pd
+import scipy
 import nibabel as nb
-import PIL
+import PIL 
 
 from transformers import SamModel
 import torch
+from torch.utils.data import DataLoader
 import torch.nn.functional as F
-from segment_anything.utils.transforms import ResizeLongestSide
+
+from src.SAMProcessing import SAMProcessing
+from src.SAMMisc import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -30,46 +32,18 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # prediction functions
 ######################
 
-def _process_box(box, shape, original_size=None, box_extension=0):
-    if box_extension == 0:  # no extension
-        extension_y, extension_x = 0, 0
-    elif box_extension >= 1:  # extension by a fixed factor
-        extension_y, extension_x = box_extension, box_extension
-    else:  # extension by fraction of the box len
-        len_y, len_x = box[2] - box[0], box[3] - box[1]
-        extension_y, extension_x = box_extension * len_y, box_extension * len_x
-
-    box = np.array([
-        max(box[1] - extension_x, 0), max(box[0] - extension_y, 0),
-        min(box[3] + extension_x, shape[1]), min(box[2] + extension_y, shape[0]),
-    ])
-
-    if original_size is not None:
-        trafo = ResizeLongestSide(max(original_size))
-        box = trafo.apply_boxes(box[None], (256, 256)).squeeze()
-    return box
-
-# compute the bounding box from a mask. SAM expects the following input:
-# box (np.ndarray or None): A length 4 array given a box prompt to the model, in XYXY format.
-def _compute_box_from_mask(mask, original_size=None, box_extension=0):
-    coords = np.where(mask == 1)
-    min_y, min_x = coords[0].min(), coords[1].min()
-    max_y, max_x = coords[0].max(), coords[1].max()
-    box = np.array([min_y, min_x, max_y + 1, max_x + 1])
-    return _process_box(box, mask.shape, original_size=original_size, box_extension=box_extension)
-
 def upsample_mask(pred_mask, target_size):
     return F.interpolate(pred_mask.unsqueeze(1), size=target_size, mode="nearest").squeeze(1)
 
-def forward_pass(model: SamModel, batch, prompt_type: str):
-    if prompt_type == 'point':
+def forward_pass(model: SamModel, batch, prompt_type: PromptType):
+    if prompt_type == PromptType.CONTROL_POINTS:
         outputs = model(
             pixel_values=batch["pixel_values"],
             input_points=batch["input_points"],
             input_labels=batch["input_labels"],
             multimask_output=False,
         )
-    elif prompt_type == 'bbox':
+    elif prompt_type == PromptType.BOUNDING_BOX:
         outputs = model(
             pixel_values=batch["pixel_values"],
             input_boxes=batch["input_boxes"],
@@ -85,16 +59,14 @@ def calc_datapoint_metrics(y_true_mask, y_pred_mask):
     if hd == np.Inf:
         hd = np.NaN
     return {
+        # "f1_score": f1_score(y_true, y_pred, zero_division=0),
         "precision": precision_score(y_true, y_pred, zero_division=0),
         "dice": 1-dice(y_true,y_pred),
         "hausdorff": hd
 
     }
 
-# m = filename of a mask of the target in the image to be segmented, can be used to make a bounding box prompt
-# t = filename of target image to be segmented
-# predictor = instance of facebook SamPredictor class
-def predict_model(model: SamModel, batch, confidence_threshold=0.5):
+def predict_model(model: SamModel, batch, prompt_args, confidence_threshold=0.5):
 
     batch = {k: v.to(device) for k, v in batch.items()}
     with torch.no_grad():
@@ -107,42 +79,34 @@ def predict_model(model: SamModel, batch, confidence_threshold=0.5):
 
     predicted_mask = torch.sigmoid(upsample_mask(raw_predicted_masks, ground_truth_mask.shape)).squeeze()
     raw_mask = predicted_mask.cpu().numpy()
-    mask = (raw_mask > confidence_threshold).astype(np.uint8)
-    metrics = calc_datapoint_metrics(ground_truth_mask.numpy().astype(np.uint8), mask)
+    mask = (raw_mask > confidence_threshold).astype(np.uint8) * 255
+    # metrics = calc_datapoint_metrics(ground_truth_mask.numpy().astype(np.uint8), mask)
+    metrics = None
     
     mask_comb = None
-    if True:
+    if False:
         bbox = batch['input_boxes'][0][0].cpu().numpy().astype('int')
         bbox_shape = tuple(batch['reshaped_input_sizes'][0].cpu().numpy())
         mask_comb = predict_plot(ground_truth_mask.numpy().astype(np.uint8),mask,bbox,bbox_shape)
 
     return raw_mask, mask, metrics, mask_comb
 
-# m = filename of a mask of the target in the image to be segmented, can be used to make a bounding box prompt
-# t = filename of target image to be segmented
-# predictor = instance of facebook SamPredictor class
-def predict_metrics(m,t,model: SamModel):
+
+def predict_metrics(model: SamModel, dataloader: DataLoader, prompt_args, datadir=None):
     model.to(device)
     model.eval()
 
-    iterCount = 0
     metrics = []    
     
-    batch = {}
     for idx, batch in enumerate(dataloader):
-        _, _, batch_metrics, mask_comb = predict_model(model, batch)
+        _, mask, batch_metrics, _ = predict_model(model, batch, prompt_args)
         metrics.append(batch_metrics)
 
-        if mask_comb is not None and datadir is not None:
-            ofile = os.path.join(datadir,'datacheck','mask_comb_' + str(idx).zfill(4) + '.png')
-            imsave(ofile,mask_comb,check_contrast=False)
+        if datadir is not None:
+            ofile = os.path.join(datadir,'predictions','pred_mask_' + str(idx).zfill(5) + '.png')
+            imsave(ofile,mask,check_contrast=False)
 
-    joined = defaultdict(list)
-    for metric in metrics:
-        for key in metric:
-            joined[key].append(metric[key])
-
-    return joined
+    return metrics
 
 def predict_plot(gt,pred,bbox,shape):
     comb_mask = np.zeros(shape+(3,),dtype='uint8')
@@ -154,84 +118,19 @@ def predict_plot(gt,pred,bbox,shape):
     comb_mask[rr,cc,1] = 255
     return comb_mask
 
-# main function to segment the prompt, with filename args
-# m = filename of a mask of the target in the image to be segmented, can be used to make a bounding box prompt
-# t = filename of target image to be segmented
-# model = instance of huggingface SamModel class
-def run_sam(m,t,prompt,model):
-    print(f"Processing '{t}'...")
-    slice = int(re.search('slice_([0-9]+)',t).group(1))
-    image_pil = PIL.Image.open(t)
-    image = np.atleast_3d(np.array(image_pil)[:,:,:3])
-    if image is None:
-        print(f"Could not load '{t}' as an image, skipping...")
-        return
-    mask = np.squeeze(plt.imread(m)[:,:,0]*1).astype(np.uint8)
-    if mask is None:
-        print('could not load {} as a mask, skipping...'.format(m))
-        return
-
-    if prompt == 'bbox':
-        bbox = _compute_box_from_mask(mask,box_extension=3)
-    point = np.array(list(map(int,np.mean(np.where(mask),axis=1)))) 
-    ros = np.array(np.where(image[:,:,0]))
-    distances = np.sqrt(np.power(ros[0]-point[0],2)+np.power(ros[1]-point[1],2))
-    arg = np.argsort(np.abs(distances - np.median(distances)))[0]
-    point_bg = np.atleast_2d(np.flip(ros[:,arg])) # flip for xy
-    point = np.atleast_2d(np.flip(point))
-    if False:
-        # optional. add some background points using a convex hull around the mask
-        convexhull = _compute_hull_from_mask(mask,hull_extension=2)
-        convexhull_points = np.concatenate((np.atleast_2d(point),convexhull),axis=0)
-        convexhull_labels = np.array([1]+[0]*len(convexhull))
-
-    # points prompt
-    # points = np.concatenate((point,point_bg),axis=0)
-    points = point # no background, just single foreground point
-    # points_label = np.array([1,0]) # foreground,background
-    points_labels = np.array([1]) # foreground only
-    batch = {}
-    _, _, batch_metrics, mask_comb = predict_model(model, batch)
-
-
-    mask_sizes = np.zeros_like(scores)
-    for i,m in enumerate(masks):
-        mask_sizes[i] = len(np.where(m)[0])
-    min_mask = np.where(mask_sizes == min(mask_sizes))[0]
-    if len(min_mask > 0):
-        min_mask = min_mask[0]
-    if np.argmax(scores) == min_mask:
-        sam_mask_point = np.copy(masks[np.argmax(scores),:,:])
-    else:
-        sam_mask_point = np.copy(masks[min_mask,:,:])
-
-    if False:
-        fig, ax = plt.subplots(1, 4, num=7, figsize=(10, 2.5),sharex=True,sharey=True)
-        ax[0].cla()
-        ax[0].imshow(image,origin='lower')
-        show_box(bbox, ax[0])
-        show_points(convexhull_points,convexhull_labels,ax[0],marker_size=10)
-        ax[0].set_title("Input Image and Bounding Box")
-        ax[1].cla()
-        ax[1].imshow(image,origin='lower')
-        show_mask(sam_mask_point[slice], ax[1])
-        ax[1].set_title('points')
-        ax[2].cla()
-        ax[2].imshow(image,origin='lower')
-        show_mask(sam_mask_box[slice],ax[2])
-        ax[3].cla()
-        ax[3].imshow(image,origin='lower')
-        show_mask(sam_mask_boxpoint[slice],ax[3])
-        ax[3].set_title('box-points')
-        plt.show(block=False)
-        a=1
-
-    return (sam_predict)
-
 ################
 # misc functions
 ################
-    
+         
+def load_model_checkpoint(checkpoint_path, model: SamModel):
+    checkpoint = torch.load(checkpoint_path)
+
+    model.load_state_dict(checkpoint["model_state_dict"])
+
+    print(f"Model checkpoint loaded from: {checkpoint_path}")
+
+    return model
+
 # load a single nifti file
 def loadnifti(t1_file,dir,type=None):
     img_arr_t1 = None
@@ -259,6 +158,7 @@ def writenifti(img_arr,filename,header=None,norm=False,type='float64',affine=Non
     if True:
         os.system('gzip --force "{}"'.format(filename))
 
+
 #############
 # script main
 #############
@@ -266,61 +166,73 @@ def writenifti(img_arr,filename,header=None,norm=False,type='float64',affine=Non
 def main(args):
 
     prompt_args = {'point':{    # Control points version:
-        "prompt_type": 'point',
+        "prompt_type": PromptType.CONTROL_POINTS,
         "num_positive": 1, # hard-coded for single point now
         "num_negative": 0, # no background points
         "erode": True,
         "multi_mask": "mean",
         },
         'bbox':{ # Bounding boxes version:
-        "prompt_type": 'bbox',
+        "prompt_type": PromptType.BOUNDING_BOX,
         "perturbation": 0,
         "padding": 3,
         "multi_mask": None
         }
     }
 
-    model = SamModel.from_pretrained(args.pretrained)
+    if args.pretrained != "None":
+        # getting a path error on this syntax
+        model = SamModel.from_pretrained(args.pretrained)
+    else:
+        # this loads from cache after 1st download
+        model = SamModel.from_pretrained(f"facebook/sam-vit-base")
     if args.checkpoint is not None:
         checkpoint = torch.load(args.checkpoint)
         model.load_state_dict(checkpoint["model_state_dict"])
     model.to(device)
 
-    datadir = "C:\\Users\\Chris Heyn Lab\\data\\brats2024\\raw"
-    test_datadir = os.path.join(datadir,'test_slice')
-    datadirs = {'test':test_datadir}
-
-    studydir = os.listdir(args.input)
+    studydir = os.listdir(args.input)[0] # hard-coded single study dir here, ie S0001
     spath = os.path.join(args.input,studydir,'sam')
     if not os.path.exists(spath):
         raise NotADirectoryError
     files = os.listdir(spath)
     if len(files) == 0:
         raise FileNotFoundError
-    # for ch,img in zip(['flair','t1+'],['WT','ET']):
-    # by definition SAM output from t1+ input is TC, not just ET
-    for ch,img in zip(['t1+'],['TC']):
-        images = [ f for f in files if re.match('slice_[0-9]+_'+ch,f) ]
-        images = sorted([os.path.join(spath, f) for f in images])
-        masks = [ f for f in files if re.match('mask_[0-9]+_'+ch,f)]
-        masks = sorted([os.path.join(spath, f) for f in masks])
-        image_pil = PIL.Image.open(images[0])
-        slicedim = int(image_pil.info['slicedim'])
-        affine_enc = image_pil.info['affine'].encode()
-        affine_dec = affine_enc.decode('unicode-escape').encode('ISO-8859-1')[2:-1]
-        affine = np.reshape(np.frombuffer(affine_dec, dtype=np.float64),(4,4))
-        sam_predict = np.zeros((slicedim,)+image_pil.size[-1::-1])
+    
+    eval_datadir = {'eval':spath}
+    samp = SAMProcessing(eval_datadir,model_size='base',prompt_type=prompt_args[args.prompt]['prompt_type'])
 
-        # for m,t in zip([masks[0]],[images[0]]):
-        for m,t in zip(masks,images):
-            print(f"Processing '{t}'...")
-            res = run_sam(m,t,predictor,)
-            res = predict_metrics(m,t,model)
-            slice = int(re.search('slice_([0-9]+)',t).group(1))
+    res = predict_metrics(model,samp.dataloaders['eval'],prompt_args[args.prompt],datadir=spath)
 
-            sam_predict[slice] = res
+    # gather the sam-predicted 2d slices into a nifti volume
+    # with the torch DataLoader, the output masks for a set of image files are just in 
+    # plain index order. map these back to the slice positions according to the filenames
+    # of the input image files. it is thus assumed that the DataLoader is processing the image
+    # files in a sorted order since it is batchsize=1 and no shuffling (to be verified)
+    img_files = os.listdir(os.path.join(spath,'images'))
+    images = sorted([ f for f in img_files if re.match('img.*slice_[0-9]{3}',f) ])
+    image_pil = PIL.Image.open(os.path.join(spath,'images',images[0]))
+    # for lack of any better way to pass to this standalone script, these values are encoded in the .png header
+    # when exported form the viewer. this script should be a method in the CreateSAMROIFrame.class
+    slicedim = int(image_pil.info['slicedim'])
+    affine_enc = image_pil.info['affine'].encode()
+    affine_dec = affine_enc.decode('unicode-escape').encode('ISO-8859-1')[2:-1]
+    affine = np.reshape(np.frombuffer(affine_dec, dtype=np.float64),(4,4))
+    sam_predict = np.zeros((slicedim,)+image_pil.size[-1::-1],dtype='uint8')
 
-        writenifti(sam_predict,os.path.join(args.output,s,'{}_sam_{}_{}.nii'.format(img,args.tag,args.prompt)),type=np.uint8,affine=affine)
+    pred_files = os.listdir(os.path.join(spath,'predictions'))
+    pred_masks = sorted([ f for f in pred_files if re.match('pred_mask',f) ])
+    for i,m in zip(images,pred_masks):
+        slice = int(re.search('slice_([0-9]{3})',i).group(1))
+        mask_pil = PIL.Image.open(os.path.join(spath,'predictions',m))
+        mask = skimage.transform.resize(np.array(mask_pil),image_pil.size[-1::-1],order=0)
+        sam_predict[slice] = mask
+
+    if args.output == "None":
+        outputdir = os.path.join(args.input,studydir)
+    else:
+        outputdir = args.output
+    writenifti(sam_predict,os.path.join(outputdir,'{}_sam_{}_{}.nii'.format('TC',args.tag,args.prompt)),type=np.uint8,affine=affine)
 
 
 
@@ -341,6 +253,7 @@ if __name__ == "__main__":
             "Path to the directory where masks will be output. Output will be either a folder "
             "of PNGs per image or a single json with COCO-style masks."
         ),
+        default="None"
     )
     parser.add_argument(
         "--model-type",
@@ -372,3 +285,4 @@ if __name__ == "__main__":
     parser.add_argument("--tag",type=str, default="",help="Tag word for file naming")
     args = parser.parse_args()
     main(args)
+
---
+++ scripts/src/SAMDataset.py
@@ -0,0 +1,194 @@
+import numpy as np
+import skimage
+from skimage.io import imread,imsave
+from skimage.transform import resize
+import monai
+import glob
+import os
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from datetime import datetime, timezone, timedelta
+import gc
+from torch.utils.data import Dataset
+
+class PromptType:  
+    CONTROL_POINTS = "pts"  
+    BOUNDING_BOX = "bbox"
+
+class SAMDataset(Dataset):  
+    def __init__(  
+        self,   
+        datadir,   
+        processor,   
+        prompt_type = PromptType.BOUNDING_BOX,  
+        num_positive = 3,  
+        num_negative = 0,  
+        erode = True,  
+        multi_mask = "mean",  
+        perturbation = 0,
+        padding = 3,  
+        image_size = (1024, 1024),  
+        mask_size = (256, 256),  
+    ):  
+        # Asign all values to self  
+        # self.dataset = dataset
+        self.datadir = datadir
+        self.dataset = os.listdir(os.path.join(self.datadir,'images'))
+        self.processor = processor
+        self.prompt_type = prompt_type
+        self.image_size = image_size
+        self.mask_size = mask_size
+
+        # These should only be used for CONTROL_POINTS prompts.
+        self.num_positive = num_positive
+        self.num_negative = num_negative
+        self.erode = erode
+        self.multi_mask = multi_mask
+
+        # This is only to be used for BOUNDING_BOX prompts.
+        self.perturbation = perturbation
+        self.padding = padding
+
+    def __len__(self):  
+        return len(self.dataset)
+
+    def __getitem__(self, idx):  
+
+        if False: # former syntax
+            ifile = glob.glob(os.path.join(self.datadir,'images','img_' + str(idx).zfill(5) + '_case_???_slice_???.png'))[0]
+        else: # new syntax
+            ifile = glob.glob(os.path.join(self.datadir,'images','img_' + str(idx).zfill(5) + '_case_*_slice_???.png'))[0]
+
+        input_image = imread(ifile)
+        input_image1 = skimage.transform.resize(input_image,self.image_size)
+        # huggingface transformers SamImageProcessor claims to support channel dim
+        # first or last and infer from shape of data, but last didn't work and 
+        # first did. 
+        # standalone sam preprocess script outputs a gray-scale image which has to be tiled back to rgb
+        # viewer code currently outputs a RGBA image, in the 3rd dim, which has to be switched to rgb, in the first dim
+        if len(np.shape(input_image1)) == 3:
+            input_image1 = np.tile(input_image1[:,:,0],(3,1,1))
+        elif len(np.shape(input_image1)) == 2:
+            input_image1 = np.tile(input_image1,(3,1,1))
+        ifile = glob.glob(os.path.join(self.datadir,'labels','img_' + str(idx).zfill(5) + '_case_*_slice_???.png'))[0]
+        # again, masks prepared by viewer code are currently rgba, float. 
+        ground_truth_mask = imread(ifile,as_gray=True).astype('uint8') * 255
+        # note. skimage resize is one of the many resize algorithms that has an easy option for
+        # nearest neighbour. so many others do not.
+        ground_truth_mask = skimage.transform.resize(ground_truth_mask,self.mask_size,order=0)
+
+        if self.prompt_type == PromptType.CONTROL_POINTS:  
+            inputs = self._getitem_ctrlpts(input_image1, ground_truth_mask)  
+        elif self.prompt_type == PromptType.BOUNDING_BOX:  
+            inputs = self._getitem_bbox(input_image1, ground_truth_mask)
+
+        inputs["ground_truth_mask"] = ground_truth_mask  
+
+        # debug plotting
+        if False:
+            # note matplotlib 3.9.0 vs code 1.92.2 plts stopped showing in debug console,
+            # until using plt.show(block=True), which is the default corresponding to
+            # plt.show(), but neither plt.show() nor plt.show(block=False work
+            check_image = np.copy(inputs['pixel_values'])[0]
+            bbox = np.array(inputs['input_boxes'][0]).astype('int')
+            check_mask = np.copy(ground_truth_mask).astype('uint8')
+
+            if False: # additionally save to file. 
+                # 32 bit tiffs
+                if False:
+                    vmax = np.max(check_image)
+                    rr,cc = skimage.draw.rectangle_perimeter(bbox[1::-1],end=bbox[:1:-1],shape=check_image.shape)
+                    check_image[rr,cc] = vmax
+                    check_mask = resize(check_mask,np.shape(check_image),order=0).astype('float32') * vmax
+                    check_comb = np.concatenate((check_image,check_mask),axis=1)
+                    ofile = os.path.join(self.datadir,'datacheck','comb_' + str(idx).zfill(4) + '.tiff')
+                    cv2.imwrite(ofile,check_comb)
+                else:
+                # 8 bit pngs
+                    check_image2 = (skimage.transform.resize(input_image,np.shape(check_image))*255).astype('uint8')
+                    check_mask = resize(check_mask,np.shape(check_image2),order=0).astype('uint8') * 255
+                    rr,cc = skimage.draw.rectangle_perimeter(bbox[1::-1],end=bbox[:1:-1],shape=check_mask.shape)
+                    check_image2[rr,cc] = 255
+                    check_comb = np.concatenate((check_image2,check_mask),axis=1).astype('uint8')
+                    ofile = os.path.join(self.datadir,'datacheck','comb_' + str(idx).zfill(4) + '.png')
+                    imsave(ofile,check_comb)
+
+        return inputs
+    
+    # control points option. single point only for now.
+    def generate_input_points(self,num_positive=1,num_negative=0,mask=None,dynamic_distance=True,erode=True):
+        input_points = []
+        row,col = map(int,np.mean(np.where(mask==255),axis=1))
+        # need [col,row]
+        input_points.append([col,row]) 
+        input_points = np.array(input_points)
+        input_labels = np.array([1])
+
+        return  input_points,input_labels         
+
+    def _getitem_ctrlpts(self, input_image, ground_truth_mask):  
+        # Get control points prompt. See the GitHub for the source  
+        # of this function, or replace with your own point selection algorithm.  
+        input_points, input_labels = self.generate_input_points(  
+            mask=ground_truth_mask  
+        )  
+        input_points = input_points.astype(float).tolist()  
+        input_labels = input_labels.tolist()  
+        input_labels = [[x] for x in input_labels]
+
+        # Prepare the image and prompt for the model.  
+        inputs = self.processor(  
+            input_image,  
+            input_points=input_points,  
+            input_labels=input_labels,  
+            return_tensors="pt"  
+        )
+
+        # Remove batch dimension which the processor adds by default.  
+        inputs = {k: v for k, v in inputs.items()}
+        # modified the original code, no longer squeezing input_points because that throws a tensor error
+        for k in inputs.keys():  
+            if k != 'input_points':
+                inputs[k] = inputs[k].squeeze(0)
+        # modified the original code, squeezing labels throws a tensor error
+        if False:
+            inputs["input_labels"] = inputs["input_labels"].squeeze(1)
+
+        return inputs
+
+    # bbox option
+    def get_input_bbox(self,mask, perturbation=0):
+        # Find minimum mask bounding all included mask points.
+        y_indices, x_indices = np.where(mask > 0)
+        x_min, x_max = np.min(x_indices), np.max(x_indices)
+        y_min, y_max = np.min(y_indices), np.max(y_indices)
+
+        # add padding
+        ydim,xdim = np.shape(mask)
+        x_min = max(0,x_min - self.padding)
+        y_min = max(0,y_min - self.padding)
+        x_max = min(xdim-1,x_max + self.padding)
+        y_max = min(ydim-1,y_max + self.padding)
+
+        if perturbation:  # Add perturbation to bounding box coordinates.
+            H, W = mask.shape
+            x_min = max(0, x_min + np.random.randint(-perturbation, perturbation))
+            x_max = min(W, x_max + np.random.randint(-perturbation, perturbation))
+            y_min = max(0, y_min + np.random.randint(-perturbation, perturbation))
+            y_max = min(H, y_max + np.random.randint(-perturbation, perturbation))
+
+        if True: # original code
+            bbox = [x_min, y_min, x_max, y_max]
+        else: # apparent correction for sam bbox convention is needed
+            bbox = [y_min, x_min, y_max, x_max]
+        return bbox
+
+    def _getitem_bbox(self, input_image, ground_truth_mask):  
+        # Get bounding box prompt.  
+        bbox = self.get_input_bbox(ground_truth_mask, perturbation=self.perturbation)
+
+        # Prepare the image and prompt for the model.  
+        inputs = self.processor(input_image, input_boxes=[[bbox]], return_tensors="pt")  
+        inputs = {k: v.squeeze(0) for k, v in inputs.items()} # Remove batch dimension which the processor adds by default.
+
+        return inputs
